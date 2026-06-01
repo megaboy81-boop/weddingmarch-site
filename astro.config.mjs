f0c785213051fd4d5161caee9e89c96ae8edf812
메가보이 <megaboy@megaboiui-MacBookAir.local>
2026-06-01 13:15:15 +0900

    fix: use http canonical while certificate is pending

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://ccweddingmarch.com',
+  site: 'http://ccweddingmarch.com',
   base: '/',
   integrations: [tailwind()],
   output: 'static',
